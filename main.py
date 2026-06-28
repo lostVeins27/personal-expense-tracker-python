a52903277277c8aea7aa921ceb8a993e5e8d315f
Daniel Doquillo <doquillodaniel@gmail.com>
2026-06-28 17:40:15 +0800

    add balance window

--- main.py
+++ main.py
@@ -6,7 +6,31 @@ app.title("Personal Expense Tracker")
 
 app.geometry("350x300")
 
+
+app.resizable(False,False)
 app.columnconfigure((0, 1, 2), weight=1)
+
+def balance_window():
+    new_window = ctk.CTkToplevel(app)
+    new_window.geometry("250x100")
+    new_window.title("Set Balance")
+    new_window.resizable(False,False)
+
+    new_window.transient(app)
+    new_window.after(10, new_window.lift)
+    new_window.after(10, new_window.focus_force)
+
+    ctk.CTkLabel(new_window, text="Enter your balance").pack(pady= (10, 10))
+
+    balance_entry = ctk.CTkEntry(new_window)
+    balance_entry.pack()
+
+    def submit(event):
+        balance = balance_entry.get()
+        balance_label.configure(text = f"BALANCE: {balance}")
+        new_window.destroy()
+
+    balance_entry.bind("<Return>", submit)
 
 
 balance_label = ctk.CTkLabel(app, text="BALANCE:", font=("Arial", 24))
@@ -18,11 +42,8 @@ add_expense_button.grid(row = 1, column = 0, padx = (10, 3), sticky="nsew")
 history_button = ctk.CTkButton(app, text="History")
 history_button.grid(row = 1, column = 1, sticky = "nsew")
 
-set_balance_button = ctk.CTkButton(app, text="Set Balance")
+set_balance_button = ctk.CTkButton(app, text="Set Balance", command= balance_window)
 set_balance_button.grid(row = 1, column = 2, padx = (3, 10), sticky = "nsew")
-
-
-app.resizable(False,False)
 
 
 app.mainloop()
