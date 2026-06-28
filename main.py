175818e690720c489d39740d32fe71fcfdde54f3
Daniel Doquillo <doquillodaniel@gmail.com>
2026-06-28 16:56:50 +0800

    WIP add grid layout

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ app.title("Personal Expense Tracker")
 
 app.geometry("350x300")
 
-balance_label = ctk.CTkLabel(app, text="BALANCE: ")
-balance_label.pack()
+app.columnconfigure((0, 1, 2), weight=1)
 
-Add_Expense_Button = ctk.CTkButton(app, text="Add Expense")
-Add_Expense_Button.pack()
+
+balance_label = ctk.CTkLabel(app, text="BALANCE:", font=("Arial", 24))
+balance_label.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = (20, 20), sticky = "w")
+
+add_expense_button = ctk.CTkButton(app, text="Add Expense")
+add_expense_button.grid(row = 1, column = 0, padx = (10, 3), sticky="nsew")
 
 history_button = ctk.CTkButton(app, text="History")
-history_button.pack()
+history_button.grid(row = 1, column = 1, sticky = "nsew")
+
+set_balance_button = ctk.CTkButton(app, text="Set Balance")
+set_balance_button.grid(row = 1, column = 2, padx = (3, 10), sticky = "nsew")
 
 
 app.resizable(False,False)
