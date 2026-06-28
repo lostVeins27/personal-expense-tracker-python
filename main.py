1aaad791da494dc74479be3f0edb1c5ce8b929c1
Daniel Doquillo <doquillodaniel@gmail.com>
2026-06-28 20:14:47 +0800

    add transaction button

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app = ctk.CTk()
 
 app.title("Personal Expense Tracker")
 
-app.geometry("350x300")
+app.geometry("400x300")
 
 
 app.resizable(False,False)
-app.columnconfigure((0, 1, 2), weight=1)
+app.columnconfigure((0, 1, 2, 3), weight=1)
 
 def balance_dialog():
     dialog = ctk.CTkInputDialog(text="Input your current balance", title="Balance")
@@ -20,7 +20,7 @@ def balance_dialog():
     balance_label.configure(text= f"BALANCE: {balance}")
 
 balance_label = ctk.CTkLabel(app, text="BALANCE: 0", font=("Arial", 24))
-balance_label.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = (20, 20), sticky = "w")
+balance_label.grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = (20, 20), sticky = "w")
 
 add_expense_button = ctk.CTkButton(app, text="Add Expense")
 add_expense_button.grid(row = 1, column = 0, padx = (10, 3), sticky="nsew")
@@ -29,7 +29,10 @@ history_button = ctk.CTkButton(app, text="History")
 history_button.grid(row = 1, column = 1, sticky = "nsew")
 
 set_balance_button = ctk.CTkButton(app, text="Set Balance", command= balance_dialog)
-set_balance_button.grid(row = 1, column = 2, padx = (3, 10), sticky = "nsew")
+set_balance_button.grid(row = 1, column = 2, padx = (3, 0), sticky = "nsew")
+
+transaction_button = ctk.CTkButton(app, text="Transaction")
+transaction_button.grid(row = 1, column = 3, padx = (3 , 10), sticky = "nsew")
 
 
 app.mainloop()
