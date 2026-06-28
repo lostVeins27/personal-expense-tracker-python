8547b5f7c54811e30f4974865fc266a446bd8b9d
Daniel Doquillo <doquillodaniel@gmail.com>
2026-06-28 20:56:44 +0800

    add history scrollable frame

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ app.title("Personal Expense Tracker")
 app.geometry("400x300")
 
 
-app.resizable(False,False)
+app.resizable(False, False)
 app.columnconfigure((0, 1, 2, 3), weight=1)
+app.rowconfigure(2, weight= 1)
+
+history = []
 
 def balance_dialog():
+
     dialog = ctk.CTkInputDialog(text="Input your current balance", title="Balance")
     balance = dialog.get_input()
 
-    if balance == None:
+    if balance is None:
         return
 
     balance_label.configure(text= f"BALANCE: {balance}")
+
 
 balance_label = ctk.CTkLabel(app, text="BALANCE: 0", font=("Arial", 24))
 balance_label.grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = (20, 20), sticky = "w")
@@ -33,6 +38,9 @@ set_balance_button.grid(row = 1, column = 2, padx = (3, 0), sticky = "nsew")
 
 transaction_button = ctk.CTkButton(app, text="Transaction")
 transaction_button.grid(row = 1, column = 3, padx = (3 , 10), sticky = "nsew")
+
+history_scrollableframe = ctk.CTkScrollableFrame(app)
+history_scrollableframe.grid(row = 2, column = 0, columnspan = 4, padx = 10, pady = 10, sticky = "nsew")
 
 
 app.mainloop()
