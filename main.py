fd842ee47f240489400d7cafce333972e4f7622d
Daniel Doquillo <doquillodaniel@gmail.com>
2026-06-29 20:39:51 +0800

    WIP Add view image button after attaching reciept image  in expense window

--- main.py
+++ main.py
@@ -38,9 +38,6 @@ def expense_window():
     window.lift()
     window.focus_force()
 
-    window.columnconfigure((1), weight= 1)
-    window.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight= 1)
-
     account_options = (
         "Cash Wallet",
         "Bank Account",
@@ -92,44 +89,55 @@ def expense_window():
 
     def attach_file():
         file_path = filedialog.askopenfilename(title= "import reciept image", filetypes= [ ("Images", "*.png *.jpg *.jpeg"), ("All files", "*.*") ])
+
+        if file_path is None or file_path == "":
+            print("no attachment")
+            return
+        
+        view_image_button = ctk.CTkButton(window, text="View Image")
+        view_image_button.grid(row = 6 , column = 1, padx = (10, 0), pady = (0, 10), sticky = "ew")
+        reciept_image.grid(row = 6, column = 2, padx = (0, 10), pady = (0, 10), sticky = "ew")
+
+    window.columnconfigure((1, 2), weight= 1)
+    window.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight= 1)
     
     # Amount
     ctk.CTkLabel(window, text="Amount:").grid(row = 0, column = 0, padx = 10, pady = (10, 5))
     amount_entry = ctk.CTkEntry(window)
-    amount_entry.grid(row = 0, column = 1, padx = 10, pady = (10, 5), sticky = "ew")
+    amount_entry.grid(row = 0, column = 1, padx = 10, columnspan = 2,  pady = (10, 5), sticky = "ew")
 
     # Category
     ctk.CTkLabel(window, text="Category:").grid(row = 1, column = 0, padx = 10, pady = (0, 5))
     category_cb = ctk.CTkComboBox(window, values = category_options, state="readonly")
-    category_cb.grid(row = 1, column = 1, padx = 10, pady = (0,5), sticky = "ew")
+    category_cb.grid(row = 1, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
 
     # Account
     ctk.CTkLabel(window, text="Account:").grid(row = 2, column = 0, padx = 10, pady = (0, 5))
     account_cb = ctk.CTkComboBox(window, values=account_options, state="readonly")
-    account_cb.grid(row = 2, column = 1, padx = 10, pady = (0,5), sticky = "ew")
+    account_cb.grid(row = 2, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
 
     # Date
     ctk.CTkLabel(window, text="Date:").grid(row = 3, column = 0, padx = 10, pady= (0,5))
     date_cb = ctk.CTkComboBox(window, values = ["Today", "Pick Date"], state="readonly", command = date_option)
-    date_cb.grid(row = 3, column = 1, padx = 10, pady = (0,5), sticky = "ew")
+    date_cb.grid(row = 3, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
 
     # Merchant
     ctk.CTkLabel(window, text="Merchant:").grid(row = 4, column = 0, padx = 10, pady = (0,5))
     merchant_entry = ctk.CTkEntry(window)
-    merchant_entry.grid(row = 4, column = 1, padx = 10, pady = (0,5), sticky = "ew")
+    merchant_entry.grid(row = 4, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
 
     # Notes
     ctk.CTkLabel(window, text="Notes:").grid(row = 5, column = 0, padx = 10, pady = (0,5))
     notes_tb = ctk.CTkTextbox(window, width= 100, height = 100 )
-    notes_tb.grid(row = 5, column = 1, padx = 10, pady = (0,5), sticky = "ew")
+    notes_tb.grid(row = 5, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
 
     # Reciept
     ctk.CTkLabel(window, text="Reciept:").grid(row = 6, column = 0, padx = 10, pady = (0, 10))
     reciept_image = ctk.CTkButton(window, text="Attach Image", command= attach_file)
-    reciept_image.grid(row = 6, column = 1, padx = 10, pady = (0, 10), sticky = "ew")
+    reciept_image.grid(row = 6, column = 1, padx = 10, pady = (0, 10), columnspan = 2, sticky = "ew")
 
     button_frame = ctk.CTkFrame(window, fg_color="transparent")
-    button_frame.grid(row =  7, column = 0, columnspan = 2, padx = 10, pady = (0, 10), sticky = "ew")
+    button_frame.grid(row =  7, column = 0, columnspan = 3, padx = 10, pady = (0, 10), sticky = "ew")
     button_frame.columnconfigure((0, 1), weight= 1)
 
     save_button = ctk.CTkButton(button_frame, text = "Save")
