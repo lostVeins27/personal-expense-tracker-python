9d2ec7bf20d51d5216e98f5c2ad1198dbb62f995
Daniel Doquillo <doquillodaniel@gmail.com>
2026-06-30 07:15:34 +0800

    Add remove image button in expense window

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def expense_window():
         "Others"
     )
 
+    # Calendar window
     def open_calendar():
         calendar_window = ctk.CTkToplevel(window)
         calendar_window.geometry("300x300")
@@ -96,58 +97,71 @@ def expense_window():
             return
         
         def open_image():
+            
+            image = Image.open(file_path)
+            width, height = image.size
+
             image_window = ctk.CTkToplevel(window)
-            image_window.geometry("300x300")
+            image_window.title("Reciept Image")
+            image_window.geometry(f"{width}x{height}")
             image_window.resizable(False, False)
 
-            img = ctk.CTkImage(Image.open(file_path), size = (200, 200))
+            img = ctk.CTkImage(light_image = image, dark_image= image, size=(width, height))
+
             label = ctk.CTkLabel(image_window, text="", image= img)
-            label.pack(expand = True)
+            label.image = img
+            label.pack(fill="both", expand = True)
 
         view_image_button = ctk.CTkButton(window, text="View Image", command = open_image)
-        view_image_button.grid(row = 6 , column = 1, padx = (10, 3), pady = (0, 10), sticky = "ew")
-        reciept_image.grid(row = 6, column = 2, padx = (3, 10), pady = (0, 10), sticky = "ew")
+        view_image_button.grid(row = 6 , column = 1, padx = (10, 2), pady = (0, 10), sticky = "ew")
 
-    window.columnconfigure((1, 2), weight= 1)
+        reciept_image.configure(text="Repick Image")
+        reciept_image.grid(row = 6, column = 2, columnspan = 1, padx = 2, pady = (0, 10), sticky = "ew")
+
+        remove_image_button = ctk.CTkButton(window, text="Remove Image")
+        remove_image_button.grid(row = 6, column = 3, padx = (2, 10), pady = (0, 10), sticky = "ew")
+        
+
+    window.columnconfigure((1, 2, 3), weight= 1)
     window.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight= 1)
     
     # Amount
     ctk.CTkLabel(window, text="Amount:").grid(row = 0, column = 0, padx = 10, pady = (10, 5))
     amount_entry = ctk.CTkEntry(window)
-    amount_entry.grid(row = 0, column = 1, padx = 10, columnspan = 2,  pady = (10, 5), sticky = "ew")
+    amount_entry.grid(row = 0, column = 1, padx = 10, columnspan = 3,  pady = (10, 5), sticky = "ew")
 
     # Category
     ctk.CTkLabel(window, text="Category:").grid(row = 1, column = 0, padx = 10, pady = (0, 5))
     category_cb = ctk.CTkComboBox(window, values = category_options, state="readonly")
-    category_cb.grid(row = 1, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
+    category_cb.grid(row = 1, column = 1, padx = 10, pady = (0,5), columnspan = 3, sticky = "ew")
 
     # Account
     ctk.CTkLabel(window, text="Account:").grid(row = 2, column = 0, padx = 10, pady = (0, 5))
     account_cb = ctk.CTkComboBox(window, values=account_options, state="readonly")
-    account_cb.grid(row = 2, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
+    account_cb.grid(row = 2, column = 1, padx = 10, pady = (0,5), columnspan = 3, sticky = "ew")
 
     # Date
     ctk.CTkLabel(window, text="Date:").grid(row = 3, column = 0, padx = 10, pady= (0,5))
     date_cb = ctk.CTkComboBox(window, values = ["Today", "Pick Date"], state="readonly", command = date_option)
-    date_cb.grid(row = 3, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
+    date_cb.grid(row = 3, column = 1, padx = 10, pady = (0,5), columnspan = 3, sticky = "ew")
 
     # Merchant
     ctk.CTkLabel(window, text="Merchant:").grid(row = 4, column = 0, padx = 10, pady = (0,5))
     merchant_entry = ctk.CTkEntry(window)
-    merchant_entry.grid(row = 4, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
+    merchant_entry.grid(row = 4, column = 1, padx = 10, pady = (0,5), columnspan = 3, sticky = "ew")
 
     # Notes
     ctk.CTkLabel(window, text="Notes:").grid(row = 5, column = 0, padx = 10, pady = (0,5))
     notes_tb = ctk.CTkTextbox(window, width= 100, height = 100 )
-    notes_tb.grid(row = 5, column = 1, padx = 10, pady = (0,5), columnspan = 2, sticky = "ew")
+    notes_tb.grid(row = 5, column = 1, padx = 10, pady = (0,5), columnspan = 3, sticky = "ew")
 
     # Reciept
     ctk.CTkLabel(window, text="Reciept:").grid(row = 6, column = 0, padx = 10, pady = (0, 10))
     reciept_image = ctk.CTkButton(window, text="Attach Image", command= attach_file)
-    reciept_image.grid(row = 6, column = 1, padx = 10, pady = (0, 10), columnspan = 2, sticky = "ew")
+    reciept_image.grid(row = 6, column = 1, padx = 10, pady = (0, 10), columnspan = 3, sticky = "ew")
 
     button_frame = ctk.CTkFrame(window, fg_color="transparent")
-    button_frame.grid(row =  7, column = 0, columnspan = 3, padx = 10, pady = (0, 10), sticky = "ew")
+    button_frame.grid(row =  7, column = 0, columnspan = 4, padx = 10, pady = (0, 10), sticky = "ew")
     button_frame.columnconfigure((0, 1), weight= 1)
 
     save_button = ctk.CTkButton(button_frame, text = "Save")
@@ -157,21 +171,27 @@ def expense_window():
     cancel_button.grid(row = 0, column = 1, sticky = "ew")
 
 
+# Balance
 balance_label = ctk.CTkLabel(app, text="BALANCE: 0", font=("Arial", 24))
 balance_label.grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = (20, 20), sticky = "w")
 
+# Expense
 add_expense_button = ctk.CTkButton(app, text="Add Expense", command=expense_window)
 add_expense_button.grid(row = 1, column = 0, padx = (10, 3), sticky="nsew")
 
+# History
 history_button = ctk.CTkButton(app, text="History")
 history_button.grid(row = 1, column = 1, sticky = "nsew")
 
+# Balance
 set_balance_button = ctk.CTkButton(app, text="Set Balance", command= balance_dialog)
 set_balance_button.grid(row = 1, column = 2, padx = (3, 0), sticky = "nsew")
 
+# Transactions
 transaction_button = ctk.CTkButton(app, text="Transactions")
 transaction_button.grid(row = 1, column = 3, padx = (3 , 10), sticky = "nsew")
 
+# Scroll frame log
 history_scrollableframe = ctk.CTkScrollableFrame(app)
 history_scrollableframe.grid(row = 2, column = 0, columnspan = 4, padx = 10, pady = 10, sticky = "nsew")
 
